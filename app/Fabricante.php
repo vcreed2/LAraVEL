087e43ba12f55b68338dc7fc45cb6ebd8c8494d9
Mauricio José Rodriguez Palacios <mauriciorodpiso7@gmail.com>
2017-05-30 03:06:30 -0400

    mostrando los datos de la BD
    se modificaron los controladores para mostrar los datos de la tabla
    fabricantes

--- app/Fabricante.php
+++ app/Fabricante.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Eloquent\Model;
 class Fabricante extends Model {
 	protected $table="fabricantes";
 	protected $fillable=array('nombre','telefono');
+	
+	protected $hidden=['created_at','updated_at'];
 
 	public function vehiculos(){
 		$this->hasMany('Vehiculo');
